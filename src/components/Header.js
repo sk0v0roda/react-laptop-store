ad0819f469bdf9ae9ccd37bcaf17faf05bb604a4
sk0v0roda <steponist@mail.ru>
2023-03-13 19:47:34 +0300

    project beautified

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,29 +4,48 @@ import Navbar from 'react-bootstrap/Navbar';
 import {Link} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {removeUser} from "../store/slices/userSlice";
+import {useAuth} from "../hooks/use-auth";
+import Button from 'react-bootstrap/Button';
 
 function Header() {
     const dispatch = useDispatch();
-
-    return (
-        <>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand>Laptop-store</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Link style={{padding: 10}} to="/">Главная</Link>
-                            <Link style={{padding: 10}} to="/catalogue">Каталог</Link>
-                            <Link style={{padding: 10}} to="/login">Войти</Link>
-                            <button
-                                onClick={() => dispatch(removeUser())}
-                            >Выйти из профиля</button>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+    const {isAuth, email} = useAuth();
+    return (isAuth ?
+            <>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand>Laptop-store</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Link class="nav-link" to="/">Главная</Link>
+                                <Link class="nav-link" to="/catalogue">Каталог</Link>
+                                <Link class="nav-link" to="/login">Войти</Link>
+                                <Button variant="outline-danger"
+                                        size="sm"
+                                    onClick={() => dispatch(removeUser())}
+                                >Выйти из профиля
+                                </Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </> :
+            <>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand>Laptop-store</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Link class="nav-link" to="/">Главная</Link>
+                                <Link class="nav-link" to="/catalogue">Каталог</Link>
+                                <Link class="nav-link" to="/login">Войти</Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
     );
 }
 
